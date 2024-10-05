206b1720fbbd963cb0e2c4dea0112c54d8c2050c
Adam Byrne <adam.r.byrne@gmail.com>
2024-10-05 01:48:34 +0100

    Sort file content order in testing to guarantee order

--- tests/test_indexer.cpp
+++ tests/test_indexer.cpp
@@ -259,12 +259,16 @@ TEST(IndexerTest, SerializeIndexWithValidIndex) {
   indexer.serialize_index();
 
   // Expected output format in the index file
-  std::string expected_output = "word total file1 count1 file2 count2 ...\n"
+  std::string expected_output = "total file1 count1 file2 count2 ...\n"
                                 "file 2 file1.txt 1 file2.txt 1\n"
                                 "test 4 file1.txt 1 file2.txt 3\n";
 
   // Read the output file and compare it with the expected output
   std::string actual_output = read_file(index_file);
+
+  // WARNING: Non-deterministic order of words in the index file (OS dependent)
+  std::sort(actual_output.begin(), actual_output.end());
+  std::sort(expected_output.begin(), expected_output.end());
   EXPECT_EQ(actual_output, expected_output);
 
   // Clean up
@@ -286,10 +290,14 @@ TEST(IndexerTest, SerializeIndexWithEmptyIndex) {
   indexer.serialize_index();
 
   // Expected output format in the index file
-  std::string expected_output = "word total file1 count1 file2 count2 ...\n";
+  std::string expected_output = "total file1 count1 file2 count2 ...\n";
 
   // Read the output file and compare it with the expected output
   std::string actual_output = read_file(index_file);
+
+  // WARNING: Non-deterministic order of words in the index file (OS dependent)
+  std::sort(actual_output.begin(), actual_output.end());
+  std::sort(expected_output.begin(), expected_output.end());
   EXPECT_EQ(actual_output, expected_output);
 
   // Clean up
@@ -326,7 +334,7 @@ TEST(IndexerTest, SerializeIndexWithMultipleFiles) {
   indexer.serialize_index();
 
   // Expected output format in the index file
-  std::string expected_output = "word total file1 count1 file2 count2 ...\n"
+  std::string expected_output = "total file1 count1 file2 count2 ...\n"
                                 "data 1 file2.txt 1\n"
                                 "file 2 file1.txt 1 file2.txt 1\n"
                                 "test 5 file1.txt 1 file2.txt 4\n";
